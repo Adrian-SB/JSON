015110ff4d88cc3b42a516bf8a6725ed9bf94701
Adrian-SB <asboceta@gmail.com>
2017-03-07 22:47:21 +0100

    Ejercicio 2 Resuelto

--- Ejercicios-JSON.py
+++ Ejercicios-JSON.py
@@ -17,15 +17,15 @@ for c in categorias:
 
 #2-Solicita una categoría y muestra el número de empresas que pertenecen a dicha categoría.
 
-contar=raw_input("Dime una categoría:")
+buscar=raw_input("Dime una categoría:") # Nombre categoría sin limpiador// De Vecinos = de-vecinos
 
 asosiaciones=raiz["items"]
 
-print asosiaciones
 
 for a in asosiaciones:
     categoria2=raiz["items"][a]["categories"]
-    for c2 in categoria2:
-        if c2 == contar:
-            print "Bien"
-                #print c.count(contar)
+    for x, c in categoria2.items():
+        if buscar == c:
+            cont=len(c)
+
+print "Hay",cont,"asosiaciones pertenecientes a la categoría",buscar
